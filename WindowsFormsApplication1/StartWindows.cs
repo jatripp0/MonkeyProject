c71f72c2e047826aba5ed9da99ed68c4fbc00877
jatripp0 <jatripp0@frostburg.edu>
2015-03-27 16:02:05 -0400

    Config GUI Added

--- WindowsFormsApplication1/StartWindows.cs
+++ WindowsFormsApplication1/StartWindows.cs
@@ -16,5 +16,56 @@ namespace MonkeyProject
         {
             InitializeComponent();
         }
+
+        private void StartWindows_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void beginTrial_Click(object sender, EventArgs e)
+        {
+            MonkeyAppWindow monkeyWindow = new MonkeyAppWindow();
+            monkeyWindow.Show();
+            this.Hide();
+        }
+
+        private void randomSizeCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            if (randomSizeCheck.Checked)
+            {
+                minSizeLabel.Enabled = true;
+                circleSizeMin.Enabled = true;
+                maxSizeLabel.Enabled = true;
+                circleSizeMax.Enabled = true;
+
+                circleSizeLabel.Enabled = false;
+                circleSizeSpinner.Enabled = false;
+            }
+            else
+            {
+                minSizeLabel.Enabled = false;
+                circleSizeMin.Enabled = false;
+                maxSizeLabel.Enabled = false;
+                circleSizeMax.Enabled = false;
+
+                circleSizeLabel.Enabled = true;
+                circleSizeSpinner.Enabled = true;
+            }
+        }
+
+        private void trialManual_CheckedChanged(object sender, EventArgs e)
+        {
+            if (trialManual.Checked)
+            {
+                trialTime.Enabled = false;
+                secondsLabel.Enabled = false;
+            }
+            else
+            {
+                trialTimeLimited.Enabled = true;
+                trialTime.Enabled = true;
+                secondsLabel.Enabled = true;
+            }
+        }
     }
 }
